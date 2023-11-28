1c25490c38a4205ecf1043e62f61d84ce7aeab1d
Chubop <samuelokirby@gmail.com>
2023-11-27 20:25:02 -0500

    idk

---
+++ components/ui/auth/login-button.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+import { Button, type ButtonProps } from "@/components/ui/button";
+import { IconSpinner } from "@/components/ui/icons";
+import supabaseClient from "@/lib/supabaseClient";
+
+interface LoginButtonProps extends ButtonProps {
+  text?: string;
+  fullWidth?: boolean;
+}
+
+export function LoginButton({
+  text = "Login",
+  className,
+  ...props
+}: LoginButtonProps) {
+  const [isLoading, setIsLoading] = React.useState(false);
+  return (
+    <>
+      <Button
+        variant="outline"
+        onClick={() => {
+          setIsLoading(true);
+          supabaseClient.auth.signInWithPassword({
+            email: "email",
+            password: "password",
+          });
+        }}
+        style={props.fullWidth ? {width: '100%'} : {}}
+        disabled={isLoading}
+        className={cn(className)}
+        {...props}
+      >
+        {isLoading ? <IconSpinner className="mr-2 animate-spin" /> : null}
+        {text}
+      </Button>
+    </>
+  );
+}
