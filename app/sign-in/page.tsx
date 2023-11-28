744886e23bf7b49b20a48d2edb2f97afc7f35967
sam.kirby <samuelokirby@gmail.com>
2023-11-28 10:48:43 -0500

    removed getServerSideProps from sign-in page.tsx

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,27 +1,7 @@
-import { GetServerSideProps } from 'next';
+// pages/sign-in.tsx
+import { GetServerSideProps } from "next";
 import { auth } from "@/auth";
-import { LoginButton } from "@/components/ui/auth/login-button";
-import { Input } from "@/components/ui/input";
-import SignInForm from '@/components/ui/auth/signin-form';
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const session = await auth();
-
-  // If user is already logged in, redirect to home
-  if (session?.user) {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false,
-      },
-    }
-  }
-
-  // If not logged in, continue rendering the page
-  return {
-    props: {}, // add your own props as necessary
-  };
-};
+import SignInForm from "@/components/ui/auth/signin-form";
 
 export default function SignInPage() {
   return (
@@ -30,4 +10,3 @@ export default function SignInPage() {
     </div>
   );
 }
-
