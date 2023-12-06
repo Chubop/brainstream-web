e36048b1c32514156321b66aa854a740987a3be9
sam.kirby <samuelokirby@gmail.com>
2023-12-06 09:15:41 -0500

    added radio-group.tsx

---
+++ components/ui/radio-group.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react'
+import * as RadioGroupPrimitive from '@radix-ui/react-radio-group'
+import { cn } from '@/lib/utils'
+
+const RadioGroup = RadioGroupPrimitive.Root
+
+const RadioGroupItem = React.forwardRef<
+  React.ElementRef<typeof RadioGroupPrimitive.Item>,
+  React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item>
+>(({ className, children, ...props }, ref) => (
+  <RadioGroupPrimitive.Item
+    ref={ref}
+    className={cn(
+      'relative flex cursor-default select-none items-center rounded-sm py-1.5 px-8 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
+      className
+    )}
+    {...props}
+  >
+    <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
+      <RadioGroupPrimitive.Indicator />
+    </span>
+    {children}
+  </RadioGroupPrimitive.Item>
+))
+
+const RadioGroupIndicator = React.forwardRef<
+  React.ElementRef<typeof RadioGroupPrimitive.Indicator>,
+  React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Indicator>
+>(({ className, ...props }, ref) => (
+  <RadioGroupPrimitive.Indicator
+    ref={ref}
+    className={cn(
+      'h-3.5 w-3.5 rounded-full border-2 border-current mr-1',
+      className
+    )}
+    {...props}
+  />
+))
+
+
+export {
+  RadioGroup,
+  RadioGroupItem,
+  RadioGroupIndicator
+}
