951201ab2e75d10d4f42015ddcc98a2d343f9f98
sam.kirby <samuelokirby@gmail.com>
2023-12-08 11:42:01 -0500

    new app/api/files route.ts

--- app/api/files/route.ts
+++ app/api/files/route.ts
@@ -1,66 +1,65 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { SignedPostPolicyV4Output } from "@google-cloud/storage";
-import { Storage } from "@google-cloud/storage";
+// pages/api/upload.ts
+import { Storage } from '@google-cloud/storage';
+import { NextRequest, NextResponse } from 'next/server';
 
-// Initialize Google Cloud Storage with project credentials.
-// Ensure that environment variables are set for authentication.
-
+// Initialize Google Cloud Storage client
 const storage = new Storage({
   projectId: process.env.PROJECT_ID,
   credentials: {
     client_email: process.env.CLIENT_EMAIL,
-    private_key: process.env.PRIVATE_KEY,
+    private_key: process.env.PRIVATE_KEY?.replace(/\\n/g, '\n'),
   },
 });
 
+// Specify bucket name here
+const bucketName = process.env.BUCKET_NAME;
+const bucket = storage.bucket(bucketName || '');
 
 
-// Get the bucket from the environment variables.
-// The bucket is where the files will be stored.
-const bucket = storage.bucket(process.env.BUCKET_NAME || '');
-
-// Handler for POST requests to upload files.
-export async function POST(req: Request) {
-  // Log environment variables for debugging purposes.
-  // It's important to ensure that sensitive info like private keys are not logged in production.
-  console.log("Environment Variables:", {
-    PROJECT_ID: process.env.PROJECT_ID,
-    CLIENT_EMAIL: process.env.CLIENT_EMAIL,
-    PRIVATE_KEY: process.env.PRIVATE_KEY ? "provided" : "not provided",
-  });
-  
+export async function POST(req: NextRequest) {
   try {
-    // Parse the request body to get the file information.
-    const json = await req.json();
-    console.log("request:", json);
-    console.log("no bucket:", json.file);
-    // Create a reference to the file in the bucket.
-    const file = bucket.file(json.file as string);
-    console.log("bucket:", file);
-    // Define the options for the signed URL.
-    const options = {
-      expires: Date.now() + 5 * 60 * 1000, // Set expiration time for the signed URL (5 minutes).
-      fields: { "x-goog-meta-source": "brainstream-web" }, // Custom metadata for the upload.
-    };
+    const body = await req.formData();
+    const file = body.get('file') as File;
+    const fileName = body.get('fileName') as string;
 
-    // Generate a signed URL that allows for direct file upload.
-    const [response] = await file.generateSignedPostPolicyV4(options);
-    // Return the signed URL in the response.
-    return response;
-  } catch (error) {
-    // Handle any errors that occur during the signed URL generation.
-    // Return a JSON response with the error message and a 500 status code.
-    return new Response(JSON.stringify({ error: `Error generating signed URL" ${error}` }), {
-      status: 500,
-      headers: {
-        'Content-Type': 'application/json',
-      },
+    if (!file) {
+      return NextResponse.json({ message: "Missing file" }, { status: 400 });
+    }
+    if (!fileName) {
+      return NextResponse.json({ message: "Missing file name" }, { status: 400 });
+    }
+
+    // Create a new blob in the bucket and upload the file data.
+    const blob = bucket.file(fileName);
+    const blobStream = blob.createWriteStream({
+      resumable: false,
     });
+
+    // Convert the file to a buffer
+    const buffer = await file.arrayBuffer();
+
+    // Wrap the stream finish event in a promise
+    await new Promise<void>((resolve, reject) => {
+      blobStream.on('error', (err) => {
+        console.error(err);
+        reject(new Error("Error uploading to Google Cloud bucket"));
+      });
+
+      blobStream.on('finish', () => {
+        resolve();
+      });
+
+      blobStream.end(Buffer.from(buffer));
+    });
+
+    // After the file has been uploaded, construct the public URL
+    const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
+
+    // Return the fileName and publicUrl in the response
+    return NextResponse.json({ fileName, publicUrl }, { status: 200 });
+
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: 'Internal Server Error'}, { status: 500 });
   }
 }
-
-// You can add more handlers for other HTTP methods if needed.
-// For example, for a GET request:
-// export async function getHandler(req: NextApiRequest, res: NextApiResponse) {
-//   // Handle GET request
-// }
