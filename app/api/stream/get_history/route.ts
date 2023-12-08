1e134f767fe806b2eccd42d1265747095973340d
sam.kirby <samuelokirby@gmail.com>
2023-12-08 17:05:31 -0500

    get_history route

--- app/api/stream/get_history/route.ts
+++ app/api/stream/get_history/route.ts
@@ -1,8 +1,8 @@
 "use server";
 
 import { createSupabaseServerComponentClient } from "@/app/auth/supabaseAppRouterClient";
+import { fetcher } from "@/lib/utils";
 import { NextRequest, NextResponse } from "next/server";
-import fetch from 'node-fetch';
 
 
 export async function POST(req: NextRequest){
@@ -26,41 +26,20 @@ export async function POST(req: NextRequest){
         });
     }
 
-    // Send a POST request to the production API to initialize the stream
-    const response = await fetch(URL, {
+    // Use the fetcher function to send a POST request to the production API to initialize the stream
+    const data = await fetcher(URL, {
         method: 'POST',
         headers: {
-        'Content-Type': 'application/json',
+            'Content-Type': 'application/json',
         },
-        body: JSON.stringify(
-            { "user_id": userId,
-              "stream_id": streamId }
-        ),
+        body: JSON.stringify({ "user_id": userId, "stream_id": streamId }),
     });
+    
 
-    // If the response from the production API is not OK, log and return the error
-    if (!response.ok) {
-        console.error("Error contacting the production API. Status:", response.status, "Status Text:", response.statusText);
-        let errorBody;
-        try {
-            // Attempt to read the error body from the response
-            const errorBody = await response.text();
-            console.error("Error Body:", errorBody);
-        } catch (error) {
-            // Log any error that occurs while parsing the error response
-            console.error("Error parsing the error response:", error);
-        }
-        // Return a JSON response with the error details
-        return NextResponse.json("Error contacting the production API", {
-            status: response.status,
-            statusText: errorBody,
-        });
-    }
-    // Parse the response data as JSON and return it
-    const data = await response.json();
     return NextResponse.json(data, {
         headers: {
-        'Content-Type': 'application/json',
+            'Content-Type': 'application/json',
         },
     });
+
 }
