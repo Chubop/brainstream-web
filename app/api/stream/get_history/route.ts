69f6a098c56cc0ac89172f6b9664c43ecad611f7
sam.kirby <samuelokirby@gmail.com>
2023-12-08 17:32:17 -0500

    improved stream_history readability

--- app/api/stream/get_history/route.ts
+++ app/api/stream/get_history/route.ts
@@ -1,45 +1,27 @@
 "use server";
 
-import { createSupabaseServerComponentClient } from "@/app/auth/supabaseAppRouterClient";
-import { fetcher } from "@/lib/utils";
+import { getSupabaseUserId } from "@/lib/supabase-utils";
+import { makePostRequest, validateRequestParams } from "@/lib/utils";
 import { NextRequest, NextResponse } from "next/server";
 
 
-export async function POST(req: NextRequest){
-    // Create a Supabase client for server-side operations
-    const supabaseClient = createSupabaseServerComponentClient();
-    // Attempt to retrieve the session from Supabase and extract the user ID
-    const session = (await supabaseClient.auth.getSession()).data.session;
+export async function POST(req: NextRequest) {
+    const requestData = await req.json();
 
-    const ROUTE = "/stream/get_history"
+    const userId = await getSupabaseUserId(requestData);
+    
+    validateRequestParams(requestData, ['user_id', 'stream_id']);
+
+    const ROUTE = "/stream/get_history";
     const URL = process.env.PROD_API_URL + ROUTE;
-    console.log("URL:", URL);
-
-    let requestData = await req.json();
-    let userId = session?.user.id ?? requestData?.user_id;
-    let streamId = requestData?.stream_id; 
-
-    // If no user ID is found, return an Unauthorized response
-    if (!userId || !streamId) {
-        return new NextResponse("Missing request body parameter (expecting user_id and stream_id)", {
-            status: 401,
-        });
+    if (!URL) {
+        throw new Error('PROD_API_URL is not set');
     }
 
-    // Use the fetcher function to send a POST request to the production API to initialize the stream
-    const data = await fetcher(URL, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ "user_id": userId, "stream_id": streamId }),
-    });
-    
-
-    return NextResponse.json(data, {
-        headers: {
-            'Content-Type': 'application/json',
-        },
+    const data = await makePostRequest(URL, {
+        "user_id": userId,
+        "stream_id": requestData.stream_id,
     });
 
+    return NextResponse.json(data);
 }
