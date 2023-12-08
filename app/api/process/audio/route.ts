35afcef07c90018782d61b12d97944471d360332
sam.kirby <samuelokirby@gmail.com>
2023-12-08 17:31:51 -0500

    improved process audio route readability

--- app/api/process/audio/route.ts
+++ app/api/process/audio/route.ts
@@ -1,47 +1,27 @@
-import { createSupabaseServerComponentClient } from "@/app/auth/supabaseAppRouterClient";
-import { fetcher } from "@/lib/utils";
+import { getSupabaseUserId } from "@/lib/supabase-utils";
+import { makePostRequest, validateRequestParams } from "@/lib/utils";
 import { NextRequest, NextResponse } from "next/server";
 
+export async function POST(req: NextRequest) {
+    const requestData = await req.json();
+    const userId = await getSupabaseUserId(requestData);
 
-export async function POST(req: NextRequest){
+    validateRequestParams(requestData, ['stream_id', 'audio_name', 'audio_file_blob_name', 'category']);
 
-    // Create a Supabase client for server-side operations
-    const supabaseClient = createSupabaseServerComponentClient();
-    // Attempt to retrieve the session from Supabase and extract the user ID
-    const session = (await supabaseClient.auth.getSession()).data.session;
-
-    const ROUTE = "/process/audio"
+    const ROUTE = "/process/audio";
     const URL = process.env.PROD_API_URL + ROUTE;
+    if (!URL) {
+        throw new Error('PROD_API_URL is not set');
+    }
 
-    let requestData = await req.json();
-    let userId = session?.user.id ?? requestData?.user_id;
-    let streamId = requestData?.stream_id;
-    let audioName = requestData?.audio_name;
-    let audioFileBlobName = requestData?.audio_file_blob_name;
-    let transcriptionMethod = requestData?.transcription_method || 'DG'; // default is 'DG' 
-    let category = requestData?.category;
-
-    // Use the fetcher function to send a POST request to the production API to process the audio
-    const data = await fetcher(URL, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            "user_id": userId,
-            "stream_id": streamId,
-            "audio_name": audioName,
-            "audio_file_blob_name": audioFileBlobName,
-            "transcription_method": transcriptionMethod,
-            "category": category
-        }),
+    const data = await makePostRequest(URL, {
+        "user_id": userId,
+        "stream_id": requestData.stream_id,
+        "audio_name": requestData.audio_name,
+        "audio_file_blob_name": requestData.audio_file_blob_name,
+        "transcription_method": requestData.transcription_method || 'DG',
+        "category": requestData.category,
     });
 
-    return NextResponse.json(data, {
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
-    // expected return is { "audio_id": UUID, "message": "Audio processed and document created successfully." }
-    
+    return NextResponse.json(data);
 }
