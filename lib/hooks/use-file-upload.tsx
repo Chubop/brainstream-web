fe31e6381e77d20526d422d41e90f4156ff68223
sam.kirby <samuelokirby@gmail.com>
2023-12-08 18:25:46 -0500

    removed extension when naming but not uploading

--- lib/hooks/use-file-upload.tsx
+++ lib/hooks/use-file-upload.tsx
@@ -7,28 +7,30 @@ const useFileUpload = () => {
   const [status, setStatus] = useState<UploadStatus>('idle');
 
 
-  const uploadFile = async (file: File, fileName: string) => {
+  const uploadFile = async (file: File, fileName: string): Promise<boolean> => {
     setStatus('uploading');
     const formData = new FormData();
     formData.append('file', file);
-    if (fileName) {
-      formData.append('fileName', fileName);
-    }
-
+    const fileExtension = file.name.split('.').pop();
+    const fullFileName = fileName + (fileExtension ? '.' + fileExtension : '');
+    formData.append('fileName', fullFileName);
+  
     try {
       const response = await fetch('/api/files', {
         method: 'POST',
         body: formData,
       });
-
+  
       if (!response.ok) {
         throw new Error('File upload failed');
       }
-
+  
       setStatus('success');
+      return true; // Return true when the upload is successful
     } catch (error) {
       setStatus('error');
       console.error('Upload error:', error);
+      return false; // Return false when the upload fails
     }
   };
 
