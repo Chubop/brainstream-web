adecf08d26830427635d0a35746be90a0c1a0b66
sam.kirby <samuelokirby@gmail.com>
2023-12-11 14:48:38 -0500

    using server action

--- components/ui/new-stream-button.tsx
+++ components/ui/new-stream-button.tsx
@@ -5,44 +5,39 @@ import { fetcher } from "@/lib/utils";
 import { Button } from "./button"
 import { IconPlus, IconSpinner } from "./icons";
 import toast from "react-hot-toast";
+import { createStream } from '@/app/actions';
 
 type NewStreamButton = {
     children?: string;
 }
 
 const NewStreamButton: React.FC<NewStreamButton> = ({ children }) => {
-    const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-    const onClick = async () => {
-        setIsLoading(true);
-        try {
-          // Assuming the route is exposed as an API route in a Next.js application
-          const data = await fetcher('/api/stream/init', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ /* any necessary body content */ }),
-          });
+  const onClick = async () => {
+      setIsLoading(true);
+      try {
+        console.log("awaiting createStream...");
+        const data = await createStream();
 
-          if (data && data.stream_id) {
-            toast.success("Stream created");
-          }
-        } catch (error) {
-          // Handle errors, possibly show an error toast
-          console.error("Failed to create stream:", error);
-          toast.error("Failed to create stream");
+        if (data && data.stream_id) {
+          toast.success(`Stream ${data.stream_id} created`);
         }
-        setIsLoading(false);
-      };
+      } catch (error) {
+        // Handle errors, possibly show an error toast
+        console.error("Failed to create stream:", error);
+        toast.error("Failed to create st  ream");
+      }
+      setIsLoading(false);
+  };
 
-    return(
-        <div className="flex justify-center w-full">
-            <Button onClick={onClick} variant={"ghost"} className="bg-background text-center w-5/6">
-                {isLoading ? <IconSpinner className="mr-2 animate-spin" /> : <IconPlus className="mr-2" />} New Stream
-            </Button>
-        </div>
-    )
+  return(
+      <div className="flex justify-center w-full px-2 pb-2">
+          <Button onClick={onClick} variant={"ghost"} className={`bg-background text-center w-full ${isLoading ? "border-2 dark:border-zinc-800" : ""}`}>
+              {isLoading ? <IconSpinner className="mr-2 animate-spin" /> : <IconPlus className="mr-2" />} {children || 'New Stream'}
+          </Button>
+      </div>
+  )
 }
 
 export default NewStreamButton;
