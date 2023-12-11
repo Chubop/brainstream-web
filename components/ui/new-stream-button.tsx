704eea73c41a12eca7d428ad78dd064119f9b7f2
sam.kirby <samuelokirby@gmail.com>
2023-12-10 20:44:12 -0500

    working new-stream-button

--- components/ui/new-stream-button.tsx
+++ components/ui/new-stream-button.tsx
@@ -1,15 +1,47 @@
+"use client";
+
+import { useState } from 'react';
+import { fetcher } from "@/lib/utils";
 import { Button } from "./button"
-import { IconPlus } from "./icons";
+import { IconPlus, IconSpinner } from "./icons";
+import toast from "react-hot-toast";
 
 type NewStreamButton = {
     children?: string;
 }
 
 const NewStreamButton: React.FC<NewStreamButton> = ({ children }) => {
+    const [isLoading, setIsLoading] = useState(false);
+
+    const onClick = async () => {
+        setIsLoading(true);
+        try {
+          // Assuming the route is exposed as an API route in a Next.js application
+          const data = await fetcher('/api/stream/init', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ /* any necessary body content */ }),
+          });
+
+          if (data && data.stream_id) {
+            toast.success("Stream created");
+          }
+        } catch (error) {
+          // Handle errors, possibly show an error toast
+          console.error("Failed to create stream:", error);
+          toast.error("Failed to create stream");
+        }
+        setIsLoading(false);
+      };
+
     return(
-        <Button variant={"ghost"} className="bg-background">
-            <IconPlus className="mr-2" /> New Stream
-        </Button>
+        <div className="flex justify-center w-full">
+            <Button onClick={onClick} variant={"ghost"} className="bg-background text-center w-5/6">
+                {isLoading ? <IconSpinner className="mr-2 animate-spin" /> : <IconPlus className="mr-2" />} New Stream
+            </Button>
+        </div>
     )
 }
 
