6f8d510b8007f1d7bfbfe7a2320de65b7d79816c
Chubop <samuelokirby@gmail.com>
2023-12-13 22:25:34 -0500

    started modidying sidebar-list

--- components/sidebar-list.tsx
+++ components/sidebar-list.tsx
@@ -1,30 +1,32 @@
-import { getChats, removeChat, shareChat } from '@/app/actions'
+import { getChats, getStream, getUserDetails, removeChat, shareChat } from '@/app/actions'
 import { SidebarActions } from '@/components/sidebar-actions'
 import { SidebarItem } from '@/components/sidebar-item'
+import { Stream } from '@/lib/types';
+import { createStorage } from '@/lib/utils';
 
 export interface SidebarListProps {
   userId?: string
 }
 
 export async function SidebarList({ userId }: SidebarListProps) {
-  const chats = await getChats(userId)
+
+  const userDetails = await getUserDetails();
+  const streamIds = (userDetails.streams || []) as string[];
+  const streamObjects: Stream[] = [];
+
+  console.log("Streams:", streamIds);
+  await Promise.all(streamIds.map(async (streamId) => {
+    let stream = await getStream({stream_id: streamId});
+    streamObjects.push(stream);
+  }));
+  console.log("Stream Objects:", streamObjects);
 
   return (
     <div className="flex-1 overflow-auto">
-      {chats?.length ? (
+      {streams?.length ? (
         <div className="space-y-2 px-2">
-          {chats.map(
-            chat =>
-              chat && (
-                <SidebarItem key={chat?.id} chat={chat}>
-                  <SidebarActions
-                    chat={chat}
-                    removeChat={removeChat}
-                    shareChat={shareChat}
-                  />
-                </SidebarItem>
-              )
-          )}
+          {streams.map((stream: Stream) => (
+            <div key={stream.stream_id} style={{whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis'}}>{stream.stream_id}</div>          ))}
         </div>
       ) : (
         <div className="p-8 text-center">
@@ -33,4 +35,30 @@ export async function SidebarList({ userId }: SidebarListProps) {
       )}
     </div>
   )
+
+  // return (
+  //   <div className="flex-1 overflow-auto">
+  //     {streams?.length ? (
+  //       <div className="space-y-2 px-2">
+  //         {chats.map(
+  //           chat =>
+  //             chat && (
+  //               <SidebarItem key={chat?.id} chat={chat}>
+  //                 <SidebarActions
+  //                   chat={chat}
+  //                   removeChat={removeChat}
+  //                   shareChat={shareChat}
+  //                 />
+  //               </SidebarItem>
+  //             )
+  //         )}
+  //       </div>
+  //     ) : (
+  //       <div className="p-8 text-center">
+  //         <p className="text-sm text-muted-foreground">No recording history</p>
+  //       </div>
+  //     )}
+  //   </div>
+  // )
+
 }
