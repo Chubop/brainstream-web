df1926beef17dd0ecd87528ad55b8637e4c323a0
Chubop <samuelokirby@gmail.com>
2023-12-13 20:54:18 -0500

    added first and last name to sign up form

--- components/ui/new-stream-button.tsx
+++ components/ui/new-stream-button.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from 'react';
-import { fetcher } from "@/lib/utils";
 import { Button } from "./button"
 import { IconPlus, IconSpinner } from "./icons";
 import toast from "react-hot-toast";
@@ -15,25 +14,24 @@ const NewStreamButton: React.FC<NewStreamButton> = ({ children }) => {
   const [isLoading, setIsLoading] = useState(false);
 
   const onClick = async () => {
-      setIsLoading(true);
-      try {
-        console.log("awaiting createStream...");
-        const data = await createStream();
-
-        if (data && data.stream_id) {
-          toast.success(`Stream ${data.stream_id} created`);
-        }
-      } catch (error) {
-        // Handle errors, possibly show an error toast
-        console.error("Failed to create stream:", error);
-        toast.error("Failed to create st  ream");
+    setIsLoading(true);
+    try {
+      console.log("About to call createStream...");
+      const data = await createStream();
+  
+      if (data && data.stream_id) {
+        toast.success(`Stream ${data.stream_id} created`);
       }
-      setIsLoading(false);
+    } catch (error) {
+      console.error("Error calling createStream:", error);
+      toast.error("Failed to create stream");
+    }
+    setIsLoading(false);
   };
 
   return(
-      <div className="flex justify-center w-full px-2 pb-2">
-          <Button onClick={onClick} variant={"ghost"} className={`bg-background text-center w-full ${isLoading ? "border-2 dark:border-zinc-800" : ""}`}>
+      <div className="flex w-full justify-center px-2 pb-2">
+          <Button onClick={onClick} variant={"ghost"} className={`w-full bg-background text-center ${isLoading ? "border-2 dark:border-zinc-800" : ""}`}>
               {isLoading ? <IconSpinner className="mr-2 animate-spin" /> : <IconPlus className="mr-2" />} {children || 'New Stream'}
           </Button>
       </div>
