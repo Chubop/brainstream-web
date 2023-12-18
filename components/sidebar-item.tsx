ef4d32b6a59ed4eddbeee053e7cb13f95241dcc2
Chubop <samuelokirby@gmail.com>
2023-12-18 13:51:55 -0500

    refactored sidebar-item.tsx

--- components/sidebar-item.tsx
+++ components/sidebar-item.tsx
@@ -3,7 +3,7 @@
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
-import { type Chat } from '@/lib/types'
+import { Stream, type Chat } from '@/lib/types'
 import { cn } from '@/lib/utils'
 import { buttonVariants } from '@/components/ui/button'
 import { IconMessage, IconUsers } from '@/components/ui/icons'
@@ -14,35 +14,26 @@ import {
 } from '@/components/ui/tooltip'
 
 interface SidebarItemProps {
-  chat: Chat
+  stream: Stream
   children: React.ReactNode
 }
 
-export function SidebarItem({ chat, children }: SidebarItemProps) {
+export function SidebarItem({ stream, children }: SidebarItemProps) {
   const pathname = usePathname()
-  const isActive = pathname === chat.path
+  const isActive = pathname === stream.stream_id
 
-  if (!chat?.id) return null
+  if (!stream?.stream_id) return null
 
   return (
     <div className="relative">
       <div className="absolute left-2 top-1 flex h-6 w-6 items-center justify-center">
-        {chat.sharePath ? (
-          <Tooltip delayDuration={1000}>
-            <TooltipTrigger
-              tabIndex={-1}
-              className="focus:bg-muted focus:ring-1 focus:ring-ring"
-            >
-              <IconUsers className="mr-2" />
-            </TooltipTrigger>
-            <TooltipContent>This is a shared chat.</TooltipContent>
-          </Tooltip>
-        ) : (
+        {(
           <IconMessage className="mr-2" />
         )}
+          <IconMessage className="mr-2" />
       </div>
       <Link
-        href={chat.path}
+        href={stream.stream_id}
         className={cn(
           buttonVariants({ variant: 'ghost' }),
           'group w-full pl-8 pr-16',
@@ -51,9 +42,9 @@ export function SidebarItem({ chat, children }: SidebarItemProps) {
       >
         <div
           className="relative max-h-5 flex-1 select-none overflow-hidden text-ellipsis break-all"
-          title={chat.title}
+          title={stream.stream_name}
         >
-          <span className="whitespace-nowrap">{chat.title}</span>
+          <span className="whitespace-nowrap">{stream.stream_name}</span>
         </div>
       </Link>
       {isActive && <div className="absolute right-2 top-1">{children}</div>}
