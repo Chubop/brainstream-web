dd483aeff0ab32f520baf8e21e226f603238f84b
sam.kirby <samuelokirby@gmail.com>
2023-12-26 15:36:04 -0500

    cleaned types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -1,44 +1,41 @@
-import { type Message } from 'ai'
-
 export interface Chat extends Record<string, any> {
-  id: string
-  title: string
-  createdAt: Date
-  userId: string
-  path: string
-  messages: Message[]
-  sharePath?: string
+    id: string;
+    title: string;
+    createdAt: Date;
+    userId: string;
+    path: string;
+    messages: Message[];
+    sharePath?: string;
 }
 
 export interface Stream {
-  action_use: any[];
-  audio_files: any[];
-  audio_use: number;
-  chat_history: any;
-  created_date: Date;
-  data_files: any[];
-  embedding_token_use: number;
-  files_use: number;
-  last_updated: Date;
-  llm_completion_token_use: number;
-  llm_prompt_token_use: number;
-  llm_token_use_total: number;
-  stream_id: string;
-  stream_name: string;
-  user_id: string;
+    action_use: any[];
+    audio_files: any[];
+    audio_use: number;
+    chat_history: any;
+    created_date: Date;
+    data_files: any[];
+    embedding_token_use: number;
+    files_use: number;
+    last_updated: Date;
+    llm_completion_token_use: number;
+    llm_prompt_token_use: number;
+    llm_token_use_total: number;
+    stream_id: string;
+    stream_name: string;
+    user_id: string;
 }
 
 export interface Message {
-  additiona_kwargs: any[],
-  content: string,
-  role: string,
-  isLoading: boolean
+    additional_kwargs: any[];
+    content: string;
+    role: string;
+    isLoading: boolean;
 }
 
-
 export type ServerActionResult<Result> = Promise<
-  | Result
-  | {
-      error: string
-    }
->
+    | Result
+    | {
+          error: string;
+      }
+>;
