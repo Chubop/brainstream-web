4a683e3790874a5bc9c030d073f7b94d2ee03c40
sam.kirby <samuelokirby@gmail.com>
2023-12-26 15:37:27 -0500

    formatting

--- components/sidebar-actions.tsx
+++ components/sidebar-actions.tsx
@@ -1,131 +1,136 @@
-'use client'
+'use client';
 
-import * as React from 'react'
-import { useRouter } from 'next/navigation'
-import { toast } from 'react-hot-toast'
+import * as React from 'react';
+import { useRouter } from 'next/navigation';
+import { toast } from 'react-hot-toast';
 
-import { type Chat, ServerActionResult, Stream } from '@/lib/types'
-import { cn, formatDate } from '@/lib/utils'
+import { type Chat, ServerActionResult, Stream } from '@/lib/types';
+import { cn, formatDate } from '@/lib/utils';
 import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle
-} from '@/components/ui/alert-dialog'
-import { Button } from '@/components/ui/button'
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+} from '@/components/ui/alert-dialog';
+import { Button } from '@/components/ui/button';
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle
-} from '@/components/ui/dialog'
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+} from '@/components/ui/dialog';
 import {
-  IconShare,
-  IconSpinner,
-  IconTrash,
-  IconUsers
-} from '@/components/ui/icons'
-import Link from 'next/link'
-import { badgeVariants } from '@/components/ui/badge'
+    IconShare,
+    IconSpinner,
+    IconTrash,
+    IconUsers,
+} from '@/components/ui/icons';
+import Link from 'next/link';
+import { badgeVariants } from '@/components/ui/badge';
 import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger
-} from '@/components/ui/tooltip'
+    Tooltip,
+    TooltipContent,
+    TooltipTrigger,
+} from '@/components/ui/tooltip';
 
 interface SidebarActionsProps {
-  stream: Stream
-  removeChat: (args: { id: string; path: string }) => ServerActionResult<void>
-  shareChat: (chat: Chat) => ServerActionResult<Chat>
+    stream: Stream;
+    removeChat: (args: {
+        id: string;
+        path: string;
+    }) => ServerActionResult<void>;
+    shareChat: (chat: Chat) => ServerActionResult<Chat>;
 }
 
 export function SidebarActions({
-  stream,
-  removeChat,
-  shareChat
+    stream,
+    removeChat,
+    shareChat,
 }: SidebarActionsProps) {
-  const [deleteDialogOpen, setDeleteDialogOpen] = React.useState(false)
-  const [shareDialogOpen, setShareDialogOpen] = React.useState(false)
-  const [isRemovePending, startRemoveTransition] = React.useTransition()
-  const [isSharePending, startShareTransition] = React.useTransition()
-  const router = useRouter()
+    const [deleteDialogOpen, setDeleteDialogOpen] = React.useState(false);
+    const [shareDialogOpen, setShareDialogOpen] = React.useState(false);
+    const [isRemovePending, startRemoveTransition] = React.useTransition();
+    const [isSharePending, startShareTransition] = React.useTransition();
+    const router = useRouter();
 
-  const copyShareLink = React.useCallback(async (chat: Chat) => {
-    // if (!chat.sharePath) {
-    //   return toast.error('Could not copy share link to clipboard')
-    // }
+    const copyShareLink = React.useCallback(async (chat: Chat) => {
+        // if (!chat.sharePath) {
+        //   return toast.error('Could not copy share link to clipboard')
+        // }
 
-    const url = new URL(window.location.href)
-    // url.pathname = chat.sharePath
-    navigator.clipboard.writeText(url.toString())
-    setShareDialogOpen(false)
-    toast.success('Share link copied to clipboard', {
-      style: {
-        borderRadius: '10px',
-        background: '#333',
-        color: '#fff',
-        fontSize: '14px'
-      },
-      iconTheme: {
-        primary: 'white',
-        secondary: 'black'
-      }
-    })
-  }, [])
+        const url = new URL(window.location.href);
+        // url.pathname = chat.sharePath
+        navigator.clipboard.writeText(url.toString());
+        setShareDialogOpen(false);
+        toast.success('Share link copied to clipboard', {
+            style: {
+                borderRadius: '10px',
+                background: '#333',
+                color: '#fff',
+                fontSize: '14px',
+            },
+            iconTheme: {
+                primary: 'white',
+                secondary: 'black',
+            },
+        });
+    }, []);
 
-  return (
-    <>
-      <div className="space-x-1">
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Button
-              variant="ghost"
-              className="h-6 w-6 p-0 hover:bg-background"
-              onClick={() => setShareDialogOpen(true)}
-            >
-              <IconShare />
-              <span className="sr-only">Share</span>
-            </Button>
-          </TooltipTrigger>
-          <TooltipContent>Share chat</TooltipContent>
-        </Tooltip>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Button
-              variant="ghost"
-              className="h-6 w-6 p-0 hover:bg-background"
-              disabled={isRemovePending}
-              onClick={() => setDeleteDialogOpen(true)}
-            >
-              <IconTrash />
-              <span className="sr-only">Delete</span>
-            </Button>
-          </TooltipTrigger>
-          <TooltipContent>Delete chat</TooltipContent>
-        </Tooltip>
-      </div>
-      <Dialog open={shareDialogOpen} onOpenChange={setShareDialogOpen}>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Share link to chat</DialogTitle>
-            <DialogDescription>
-              Anyone with the URL will be able to view the shared chat.
-            </DialogDescription>
-          </DialogHeader>
-          <div className="space-y-1 rounded-md border p-4 text-sm">
-            <div className="font-medium">{stream.stream_name}</div>
-            <div className="text-muted-foreground">
-              {formatDate(stream.created_date)} · {stream.chat_history.length} messages
+    return (
+        <>
+            <div className="space-x-1">
+                <Tooltip>
+                    <TooltipTrigger asChild>
+                        <Button
+                            variant="ghost"
+                            className="h-6 w-6 p-0 hover:bg-background"
+                            onClick={() => setShareDialogOpen(true)}
+                        >
+                            <IconShare />
+                            <span className="sr-only">Share</span>
+                        </Button>
+                    </TooltipTrigger>
+                    <TooltipContent>Share chat</TooltipContent>
+                </Tooltip>
+                <Tooltip>
+                    <TooltipTrigger asChild>
+                        <Button
+                            variant="ghost"
+                            className="h-6 w-6 p-0 hover:bg-background"
+                            disabled={isRemovePending}
+                            onClick={() => setDeleteDialogOpen(true)}
+                        >
+                            <IconTrash />
+                            <span className="sr-only">Delete</span>
+                        </Button>
+                    </TooltipTrigger>
+                    <TooltipContent>Delete chat</TooltipContent>
+                </Tooltip>
             </div>
-          </div>
-          <DialogFooter className="items-center">
-            {/* {chat.sharePath && (
+            <Dialog open={shareDialogOpen} onOpenChange={setShareDialogOpen}>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Share link to chat</DialogTitle>
+                        <DialogDescription>
+                            Anyone with the URL will be able to view the shared
+                            chat.
+                        </DialogDescription>
+                    </DialogHeader>
+                    <div className="space-y-1 rounded-md border p-4 text-sm">
+                        <div className="font-medium">{stream.stream_name}</div>
+                        <div className="text-muted-foreground">
+                            {formatDate(stream.created_date)} ·{' '}
+                            {stream.chat_history.length} messages
+                        </div>
+                    </div>
+                    <DialogFooter className="items-center">
+                        {/* {chat.sharePath && (
               <Link
                 href={chat.sharePath}
                 className={cn(
@@ -138,7 +143,7 @@ export function SidebarActions({
                 {chat.sharePath}
               </Link>
             )} */}
-            {/* <Button
+                        {/* <Button
               disabled={isSharePending}
               onClick={() => {
                 startShareTransition(async () => {
@@ -168,50 +173,57 @@ export function SidebarActions({
                 <>Copy link</>
               )}
             </Button> */}
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-      <AlertDialog open={deleteDialogOpen} onOpenChange={setDeleteDialogOpen}>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-            <AlertDialogDescription>
-              This will permanently delete your chat message and remove your
-              data from our servers.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel disabled={isRemovePending}>
-              Cancel
-            </AlertDialogCancel>
-            <AlertDialogAction
-              disabled={isRemovePending}
-              onClick={event => {
-                event.preventDefault()
-                startRemoveTransition(async () => {
-                  const result = await removeChat({
-                    id: chat.id,
-                    path: chat.path
-                  })
-
-                  if (result && 'error' in result) {
-                    toast.error(result.error)
-                    return
-                  }
-
-                  setDeleteDialogOpen(false)
-                  router.refresh()
-                  router.push('/')
-                  toast.success('Chat deleted')
-                })
-              }}
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
+            <AlertDialog
+                open={deleteDialogOpen}
+                onOpenChange={setDeleteDialogOpen}
             >
-              {isRemovePending && <IconSpinner className="mr-2 animate-spin" />}
-              Delete
-            </AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
-    </>
-  )
+                <AlertDialogContent>
+                    <AlertDialogHeader>
+                        <AlertDialogTitle>
+                            Are you absolutely sure?
+                        </AlertDialogTitle>
+                        <AlertDialogDescription>
+                            This will permanently delete your chat message and
+                            remove your data from our servers.
+                        </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                        <AlertDialogCancel disabled={isRemovePending}>
+                            Cancel
+                        </AlertDialogCancel>
+                        <AlertDialogAction
+                            disabled={isRemovePending}
+                            onClick={(event) => {
+                                event.preventDefault();
+                                startRemoveTransition(async () => {
+                                    const result = await removeChat({
+                                        id: chat.id,
+                                        path: chat.path,
+                                    });
+
+                                    if (result && 'error' in result) {
+                                        toast.error(result.error);
+                                        return;
+                                    }
+
+                                    setDeleteDialogOpen(false);
+                                    router.refresh();
+                                    router.push('/');
+                                    toast.success('Chat deleted');
+                                });
+                            }}
+                        >
+                            {isRemovePending && (
+                                <IconSpinner className="mr-2 animate-spin" />
+                            )}
+                            Delete
+                        </AlertDialogAction>
+                    </AlertDialogFooter>
+                </AlertDialogContent>
+            </AlertDialog>
+        </>
+    );
 }
