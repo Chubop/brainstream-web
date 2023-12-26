f029bb1c56b542fc4bafac0dcf525dd1e41a2500
sam.kirby <samuelokirby@gmail.com>
2023-12-26 15:37:15 -0500

    improved sidebar

--- components/header.tsx
+++ components/header.tsx
@@ -1,73 +1,56 @@
-"use server";
+'use server';
 
-import * as React from "react";
-import Link from "next/link";
+import * as React from 'react';
+import Link from 'next/link';
 
-import { clearChats } from "@/app/actions";
-import { Button } from "@/components/ui/button";
-import { Sidebar } from "@/components/sidebar";
-import { SidebarList } from "@/components/sidebar-list";
-import {
-  IconSeparator,
-} from "@/components/ui/icons";
-import { SidebarFooter } from "@/components/sidebar-footer";
-import { ThemeToggle } from "@/components/theme-toggle";
-import { ClearHistory } from "@/components/clear-history";
-import { UserMenu } from "@/components/user-menu";
-import {
-  createSupabaseAppServerClient,
-} from "@/app/auth/supabaseAppRouterClient";
-import HeaderButtons from "./header-buttons";
-import NewStreamButton from "./ui/new-stream-button";
+import { clearChats } from '@/app/actions';
+import { Button } from '@/components/ui/button';
+import { Sidebar } from '@/components/sidebar';
+import { SidebarList } from '@/components/sidebar-list';
+import { IconSeparator } from '@/components/ui/icons';
+import { SidebarFooter } from '@/components/sidebar-footer';
+import { ThemeToggle } from '@/components/theme-toggle';
+import { ClearHistory } from '@/components/clear-history';
+import { UserMenu } from '@/components/user-menu';
+import { createSupabaseAppServerClient } from '@/app/auth/supabaseAppRouterClient';
+import HeaderButtons from './header-buttons';
+import NewStreamButton from './ui/new-stream-button';
 
 export async function Header() {
-  const supabaseClient = createSupabaseAppServerClient();
-  const session = (await supabaseClient.auth.getSession()).data.session;
+    const supabaseClient = createSupabaseAppServerClient();
+    const session = (await supabaseClient.auth.getSession()).data.session;
 
-  
-  return (
-    <header className="sticky top-0 z-50 flex h-16 w-full shrink-0 items-center justify-between border-b bg-gradient-to-b from-background/10 via-background/50 to-background/80 px-4 backdrop-blur-xl">
-      <div className="flex items-center">
-        {session?.user && (
-          <Sidebar>
-            <React.Suspense fallback={<div className="flex-1 overflow-auto" />}>
-              {/* @ts-ignore */}
-              <NewStreamButton />
-              <SidebarList userId={session?.user?.id} />
-            </React.Suspense>
-            <SidebarFooter>
-              <ThemeToggle />
-              <ClearHistory clearChats={clearChats} />
-            </SidebarFooter>
-          </Sidebar>
-        )}
-        <div className="flex items-center">
-          <IconSeparator className="h-6 w-6 text-muted-foreground/50" />
-          {session?.user ? (
-            <UserMenu user={session.user} />
-          ) : (
-            <Button variant="link" asChild className="-ml-2">
-              <Link href="/sign-in">Login</Link>
-            </Button>
-          )}
-          {/* <IconGitHub /> */}
-          <HeaderButtons />
-          {/* <HeaderButton href="/" icon={<IconHome />}>
-              Homepage
-            </HeaderButton> */}
-        </div>
-      </div>
-      <div className="flex items-center justify-end space-x-2">
-        {/* <a
-          href="https://github.com/vercel/nextjs-ai-chatbot/"
-          target="_blank"
-          className={cn(buttonVariants())}
-        >
-          <IconVercel className="mr-2" />
-          <span className="hidden sm:block">Deploy to Vercel</span>
-          <span className="sm:hidden">Deploy</span>
-        </a> */}
-      </div>
-    </header>
-  );
+    return (
+        <header className="sticky top-0 z-50 flex h-16 w-full shrink-0 items-center justify-between border-b bg-gradient-to-b from-background/10 via-background/50 to-background/80 px-4 backdrop-blur-xl">
+            <div className="flex items-center">
+                {session?.user && (
+                    <Sidebar>
+                        <React.Suspense
+                            fallback={<div className="flex-1 overflow-auto" />}
+                        >
+                            {/* @ts-ignore */}
+                            <NewStreamButton />
+                            <SidebarList userId={session?.user?.id} />
+                        </React.Suspense>
+                        <SidebarFooter>
+                            <ThemeToggle />
+                            <ClearHistory clearChats={clearChats} />
+                        </SidebarFooter>
+                    </Sidebar>
+                )}
+                <div className="flex items-center">
+                    <IconSeparator className="h-6 w-6 text-muted-foreground/50" />
+                    {session?.user ? (
+                        <UserMenu user={session.user} />
+                    ) : (
+                        <Button variant="link" asChild className="-ml-2">
+                            <Link href="/sign-in">Login</Link>
+                        </Button>
+                    )}
+                    <HeaderButtons />
+                </div>
+            </div>
+            <div className="flex items-center justify-end space-x-2"></div>
+        </header>
+    );
 }
--- components/sidebar-item.tsx
+++ components/sidebar-item.tsx
@@ -1,53 +1,51 @@
-'use client'
+'use client';
 
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
-import { Stream, type Chat } from '@/lib/types'
-import { cn } from '@/lib/utils'
-import { buttonVariants } from '@/components/ui/button'
-import { IconMessage, IconUsers } from '@/components/ui/icons'
+import { Stream, type Chat } from '@/lib/types';
+import { cn } from '@/lib/utils';
+import { buttonVariants } from '@/components/ui/button';
+import { IconMessage, IconUsers } from '@/components/ui/icons';
 import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger
-} from '@/components/ui/tooltip'
+    Tooltip,
+    TooltipContent,
+    TooltipTrigger,
+} from '@/components/ui/tooltip';
 
 interface SidebarItemProps {
-  stream: Stream
-  children: React.ReactNode
+    stream: Stream;
+    children: React.ReactNode;
 }
 
 export function SidebarItem({ stream, children }: SidebarItemProps) {
-  const pathname = usePathname()
-  const isActive = pathname === stream.stream_id
+    const pathname = usePathname();
+    const isActive = pathname === stream.stream_id;
 
-  if (!stream?.stream_id) return null
+    if (!stream?.stream_id) return null;
 
-  return (
-    <div className="relative">
-      <div className="absolute left-2 top-1 flex h-6 w-6 items-center justify-center">
-        {(
-          <IconMessage className="mr-2" />
-        )}
-          <IconMessage className="mr-2" />
-      </div>
-      <Link
-        href={"/stream/" + stream.stream_id}
-        className={cn(
-          buttonVariants({ variant: 'ghost' }),
-          'group w-full pl-8 pr-16',
-          isActive && 'bg-accent'
-        )}
-      >
-        <div
-          className="relative max-h-5 flex-1 select-none overflow-hidden text-ellipsis break-all"
-          title={stream.stream_name}
-        >
-          <span className="whitespace-nowrap">{stream.stream_name}</span>
+    return (
+        <div className="relative">
+            <Link
+                href={'/stream/' + stream.stream_id}
+                className={cn(
+                    buttonVariants({ variant: 'ghost' }),
+                    'group w-full pl-4 pr-16',
+                    isActive && 'bg-accent'
+                )}
+            >
+                <div
+                    className="relative max-h-5 flex-1 select-none overflow-hidden text-ellipsis break-all"
+                    title={stream.stream_name}
+                >
+                    <span className="whitespace-nowrap">
+                        {stream.stream_name}
+                    </span>
+                </div>
+            </Link>
+            {isActive && (
+                <div className="absolute right-2 top-1">{children}</div>
+            )}
         </div>
-      </Link>
-      {isActive && <div className="absolute right-2 top-1">{children}</div>}
-    </div>
-  )
+    );
 }
