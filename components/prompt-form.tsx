77b93426f2d342a4db71496d249f998d48a3e852
sam.kirby <samuelokirby@gmail.com>
2023-12-26 15:36:22 -0500

    significantly improved stream creation in both button and prompt-form

--- components/prompt-form.tsx
+++ components/prompt-form.tsx
@@ -1,136 +1,138 @@
-"use client";
+'use client';
 
-import * as React from 'react'
-import Textarea from 'react-textarea-autosize'
+import * as React from 'react';
+import Textarea from 'react-textarea-autosize';
 
-import { Button, buttonVariants } from '@/components/ui/button'
-import { IconArrowElbow, IconPlus, IconUpload } from '@/components/ui/icons'
+import { Button, buttonVariants } from '@/components/ui/button';
+import { IconArrowElbow, IconPlus, IconUpload } from '@/components/ui/icons';
 import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger
-} from '@/components/ui/tooltip'
-import { useEnterSubmit } from '@/lib/hooks/use-enter-submit'
-import { cn } from '@/lib/utils'
+    Tooltip,
+    TooltipContent,
+    TooltipTrigger,
+} from '@/components/ui/tooltip';
+import { useEnterSubmit } from '@/lib/hooks/use-enter-submit';
+import { cn } from '@/lib/utils';
 import AudioUploadDialog from './audio-upload/upload-dialog';
 import { Dialog } from './ui/dialog';
 import { createSupabaseFrontendClient } from '@/app/auth/supabase';
-import { useRouter, usePathname } from 'next/navigation';
-import { createStream, sendQuery } from '@/app/actions';
-import toast from 'react-hot-toast';
-
 
 export interface PromptProps {
-  onSubmit: (value: string) => Promise<void>
-  input: string
-  setInput: (value: string) => void
-  isLoading: boolean
+    onSubmit: (value: string) => Promise<void>;
+    input: string;
+    setInput: (value: string) => void;
+    isLoading: boolean;
 }
 
 export function PromptForm({
-  onSubmit,
-  input,
-  setInput,
-  isLoading
+    onSubmit,
+    input,
+    setInput,
+    isLoading,
 }: PromptProps) {
-  const { formRef, onKeyDown } = useEnterSubmit()
-  const [isDialogOpen, setIsDialogOpen] = React.useState(false);
-  const [userId, setUserId] = React.useState<string | null>(null);
-  const inputRef = React.useRef<HTMLTextAreaElement>(null)
-  const supabaseClient = createSupabaseFrontendClient();
+    const { formRef, onKeyDown } = useEnterSubmit();
+    const [isDialogOpen, setIsDialogOpen] = React.useState(false);
+    const [userId, setUserId] = React.useState<string | null>(null);
+    const inputRef = React.useRef<HTMLTextAreaElement>(null);
+    const supabaseClient = createSupabaseFrontendClient();
 
-
-  React.useEffect(() => {
-    async function loadSession() {
-      try {
-        const { data } = await supabaseClient.auth.getSession();
-        if (data.session?.user?.id) {
-          setUserId(data.session.user.id);
+    React.useEffect(() => {
+        async function loadSession() {
+            try {
+                const { data } = await supabaseClient.auth.getSession();
+                if (data.session?.user?.id) {
+                    setUserId(data.session.user.id);
+                }
+            } catch (error) {
+                console.error('Error fetching session:', error);
+            }
         }
-      } catch (error) {
-        console.error('Error fetching session:', error);
-      }
-    }
-  
-    loadSession();
-  }, [supabaseClient.auth]);
 
-  React.useEffect(() => {
-    if (inputRef.current) {
-      inputRef.current.focus()
-    }
-  }, [])
+        loadSession();
+    }, [supabaseClient.auth]);
 
-  return (
-    <form
-      onSubmit={async e => {
-        e.preventDefault();
-        setInput('')
-        await onSubmit(input);
-        if (!input?.trim()) {
-          return
+    React.useEffect(() => {
+        if (inputRef.current) {
+            inputRef.current.focus();
         }
-      }}
-      ref={formRef}
-    >
-      <div className="relative flex max-h-60 w-full grow flex-col overflow-hidden bg-background px-8 sm:rounded-md sm:border sm:px-12">
-        
-        {isDialogOpen && (
-          <div className='m-5'>
-            <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-              <AudioUploadDialog setIsDialogOpen={setIsDialogOpen} />
-            </Dialog>
-          </div>
-        )}
+    }, []);
 
-        {userId && (
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <button
-                onClick={e => {
-                  e.preventDefault();
-                  setIsDialogOpen(true);
-                }}
-                className={cn(
-                  buttonVariants({ size: 'sm', variant: 'outline' }),
-                  'absolute left-0 top-4 h-8 w-8 rounded-full bg-background p-0 sm:left-4'
+    return (
+        <form
+            onSubmit={async (e) => {
+                e.preventDefault();
+                setInput('');
+                await onSubmit(input);
+                if (!input?.trim()) {
+                    return;
+                }
+            }}
+            ref={formRef}
+        >
+            <div className="relative flex max-h-60 w-full grow flex-col overflow-hidden bg-background px-8 sm:rounded-md sm:border sm:px-12">
+                {isDialogOpen && (
+                    <div className="m-5">
+                        <Dialog
+                            open={isDialogOpen}
+                            onOpenChange={setIsDialogOpen}
+                        >
+                            <AudioUploadDialog
+                                setIsDialogOpen={setIsDialogOpen}
+                            />
+                        </Dialog>
+                    </div>
                 )}
-              >
-                <IconUpload />
-                <span className="sr-only">New Chat</span>
-              </button>
-            </TooltipTrigger>
-            <TooltipContent>Upload an Audio File</TooltipContent>
-          </Tooltip>
-        )}
 
-        <Textarea
-          ref={inputRef}
-          tabIndex={0}
-          onKeyDown={onKeyDown}
-          rows={1}
-          value={input}
-          onChange={e => setInput(e.target.value)}
-          placeholder="Send a message."
-          spellCheck={false}
-          className="min-h-[60px] w-full resize-none bg-transparent px-4 py-[1.3rem] focus-within:outline-none sm:text-sm"
-        />
-        <div className="absolute right-0 top-4 sm:right-4">
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button
-                type="submit"
-                size="icon"
-                disabled={isLoading || input === ''}
-              >
-                <IconArrowElbow />
-                <span className="sr-only">Send message</span>
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent>Send message</TooltipContent>
-          </Tooltip>
-        </div>
-      </div>
-    </form>
-  )
+                {userId && (
+                    <Tooltip>
+                        <TooltipTrigger asChild>
+                            <button
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    setIsDialogOpen(true);
+                                }}
+                                className={cn(
+                                    buttonVariants({
+                                        size: 'sm',
+                                        variant: 'outline',
+                                    }),
+                                    'absolute left-0 top-4 h-8 w-8 rounded-full bg-background p-0 sm:left-4'
+                                )}
+                            >
+                                <IconUpload />
+                                <span className="sr-only">New Chat</span>
+                            </button>
+                        </TooltipTrigger>
+                        <TooltipContent>Upload an Audio File</TooltipContent>
+                    </Tooltip>
+                )}
+
+                <Textarea
+                    ref={inputRef}
+                    tabIndex={0}
+                    onKeyDown={onKeyDown}
+                    rows={1}
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                    placeholder="Send a message."
+                    spellCheck={false}
+                    className="min-h-[60px] w-full resize-none bg-transparent px-4 py-[1.3rem] focus-within:outline-none sm:text-sm"
+                />
+                <div className="absolute right-0 top-4 sm:right-4">
+                    <Tooltip>
+                        <TooltipTrigger asChild>
+                            <Button
+                                type="submit"
+                                size="icon"
+                                disabled={isLoading || input === ''}
+                            >
+                                <IconArrowElbow />
+                                <span className="sr-only">Send message</span>
+                            </Button>
+                        </TooltipTrigger>
+                        <TooltipContent>Send message</TooltipContent>
+                    </Tooltip>
+                </div>
+            </div>
+        </form>
+    );
 }
--- components/ui/new-stream-button.tsx
+++ components/ui/new-stream-button.tsx
@@ -1,46 +1,32 @@
-"use client";
+'use client';
 
-import { useState } from 'react';
-import { Button } from "./button"
-import { IconPlus, IconSpinner } from "./icons";
-import toast from "react-hot-toast";
-import { createStream } from '@/app/actions';
-import { useRouter } from 'next/navigation';
+import { Button } from './button';
+import { IconPlus, IconSpinner } from './icons';
+import { useCreateStream } from '@/lib/hooks/use-create-stream';
 
 type NewStreamButton = {
     children?: string;
-}
+};
 
 const NewStreamButton: React.FC<NewStreamButton> = ({ children }) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const router = useRouter();
+    const { createAndNavigate, isLoading } = useCreateStream();
 
-  const onClick = async () => {
-    setIsLoading(true);
-    try {
-      console.log("About to call createStream...");
-      const data = await createStream();
-  
-      if (data && data.stream_id) {
-        toast.success(`Stream ${data.stream_id} created`);
-        router.prefetch(`/stream/${data.stream_id}`)
-        router.push(`/stream/${data.stream_id}`);
-        router.refresh();
-      }
-    } catch (error) {
-      console.error("Error calling createStream:", error);
-      toast.error("Failed to create stream");
-    }
-    setIsLoading(false);
-  };
-
-  return(
-      <div className="flex w-full justify-center px-2 pb-2">
-          <Button onClick={onClick} variant={"ghost"} className={`w-full bg-background text-center ${isLoading ? "border-2 dark:border-zinc-800" : ""}`}>
-              {isLoading ? <IconSpinner className="mr-2 animate-spin" /> : <IconPlus className="mr-2" />} {children || 'New Stream'}
-          </Button>
-      </div>
-  )
-}
-
+    return (
+        <div className="flex w-full justify-center px-2 pb-2">
+            <Button
+                onClick={createAndNavigate}
+                disabled={isLoading}
+                variant={'ghost'}
+                className="w-full text-center"
+            >
+                {isLoading ? (
+                    <IconSpinner className="mr-2 animate-spin" />
+                ) : (
+                    <IconPlus className="mr-2" />
+                )}
+                {children || 'New Stream'}
+            </Button>
+        </div>
+    );
+};
 export default NewStreamButton;
