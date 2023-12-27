ed8c8bf8aee486018b7b3acea1eee136fff03e8a
sam.kirby <samuelokirby@gmail.com>
2023-12-27 14:47:43 -0500

    vercel deployment changes + formatting and key map error

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,27 +1,31 @@
-"use server";
+'use server';
 
-import { getSupabaseUserId } from "@/lib/supabase-utils";
-import { makePostRequest, validateRequestParams } from "@/lib/utils";
-import { NextRequest, NextResponse } from "next/server";
-
+import { getSupabaseUserId } from '@/lib/server-utils';
+import { makePostRequest, validateRequestParams } from '@/lib/utils';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest) {
     const requestData = await req.json();
     const userId = await getSupabaseUserId(requestData);
 
-    validateRequestParams(requestData, ['first_name', 'last_name', 'user_id', 'email']);
+    validateRequestParams(requestData, [
+        'first_name',
+        'last_name',
+        'user_id',
+        'email',
+    ]);
 
-    const ROUTE = "/user/init";
+    const ROUTE = '/user/init';
     const URL = process.env.PROD_API_URL + ROUTE;
     if (!URL) {
         throw new Error('PROD_API_URL is not set');
     }
 
     const data = await makePostRequest(URL, {
-        "first_name": requestData.first_name,
-        "last_name": requestData.last_name,
-        "user_id": userId,
-        "email": requestData.email
+        first_name: requestData.first_name,
+        last_name: requestData.last_name,
+        user_id: userId,
+        email: requestData.email,
     });
 
     return NextResponse.json(data);
--- components/chat-list.tsx
+++ components/chat-list.tsx
@@ -1,27 +1,31 @@
-
-import { Separator } from "@/components/ui/separator";
-import { ChatMessage } from "@/components/chat-message";
-import { Message } from "@/lib/types";
+import { Separator } from '@/components/ui/separator';
+import { ChatMessage } from '@/components/chat-message';
+import { Message } from '@/lib/types';
 
 export interface ChatList {
-  messages: Message[];
+    messages: Message[];
 }
 
 export function ChatList({ messages }: ChatList) {
-  if (!messages.length) {
-    return null;
-  }
+    if (!messages.length) {
+        return null;
+    }
 
-  return (
-    <div className="relative mx-auto max-w-2xl px-4">
-      {messages.map((message, index) => {
-        return (
-          <>
-            <ChatMessage key={index} message={message} />
-            {index < messages.length - 1 && <Separator key={message.content} className="my-4 md:my-8" />}
-          </>
-        );
-      })}
-    </div>
-  );
+    return (
+        <div className="relative mx-auto max-w-2xl px-4">
+            {messages.map((message, index) => {
+                return (
+                    <>
+                        <ChatMessage key={message.content} message={message} />
+                        {index < messages.length - 1 && (
+                            <Separator
+                                key={message.content}
+                                className="my-4 md:my-8"
+                            />
+                        )}
+                    </>
+                );
+            })}
+        </div>
+    );
 }
