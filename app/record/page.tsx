6ab51d2140d5e4a0f63cb006d17e3ea3379c20aa
sam.kirby <samuelokirby@gmail.com>
2023-12-29 18:54:28 -0500

    made RecordPage server-side

--- app/record/page.tsx
+++ app/record/page.tsx
@@ -1,21 +1,7 @@
-"use client";
 
-import { useState } from 'react';
-import { Button } from "@/components/ui/button";
-import { useUserId } from '@/lib/hooks/use-user-id';
-import { notFound } from 'next/navigation';
 import Recorder from '@/components/audio-upload/recorder';
 
 export default function RecordPage() {
-  const [isRecording, setIsRecording] = useState(false);
-  const userId = useUserId();
-
-  const handleRecording = () => {
-    // Implement recording logic here
-    setIsRecording(!isRecording);
-  };
-
-    // if (!userId) return notFound();
 
   return (
     <div className="mx-auto max-w-2xl px-4 pt-16 w-full">
