a1b92453fc42cb45e6f97c477729620e54c2b758
sam.kirby <samuelokirby@gmail.com>
2023-12-29 15:17:05 -0500

    added handleNewRecording to audio-dropdown

--- components/audio-upload/audio-dropdown.tsx
+++ components/audio-upload/audio-dropdown.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { cn } from '@/lib/utils';
 import { buttonVariants } from '../ui/button';
 import {
@@ -6,14 +8,37 @@ import {
     DropdownMenuItem,
     DropdownMenuTrigger,
 } from '../ui/dropdown-menu';
-import { IconHamburger, IconPlus, IconUpload } from '../ui/icons';
+import { IconHamburger, IconMicrophone, IconUpload } from '../ui/icons';
 import { SetStateAction } from 'react';
+import { usePathname, useRouter } from 'next/navigation';
 
 type AudioDropdownProps = {
     setIsDialogOpen: React.Dispatch<SetStateAction<boolean>>
 };
 
 export default function AudioDropdown({setIsDialogOpen}: AudioDropdownProps) {
+
+    const router = useRouter();
+    const pathname = usePathname();
+
+    // Handler for creating a new recording
+    const handleNewRecording = () => {
+        // Split the current pathname
+        const path = pathname.split("/");
+        // Check if the current page is a stream
+        console.log(path);
+        if(path[1] === "stream"){
+            // Extract the stream ID from the path
+            const streamId = path[2];
+            // Navigate to the record page with the stream ID as a query parameter
+            router.push(`/record?stream=${streamId}`)
+        }
+        else{
+            // Navigate to the record page without any query parameters
+            router.push("/record");
+        }
+    }
+
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
@@ -38,8 +63,8 @@ export default function AudioDropdown({setIsDialogOpen}: AudioDropdownProps) {
                     <IconUpload className="mr-2" />
                     Upload a Recording
                 </DropdownMenuItem>
-                <DropdownMenuItem>
-                    <IconPlus className="mr-2" />
+                <DropdownMenuItem onClick={handleNewRecording}>
+                    <IconMicrophone className="mr-2"/>
                     New Recording
                 </DropdownMenuItem>
             </DropdownMenuContent>
